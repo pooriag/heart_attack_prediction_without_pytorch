20dab36968950928157feefdb0d1ec564734395e
pog <pooriagolestanipg@gmail.com>
2024-02-14 19:44:29 +0330

    parameter b was added

--- model.py
+++ model.py
@@ -4,8 +4,8 @@ import numpy as np
 
 class logistic_model():
     def __init__(self, inp_features):
-        self.weights = np.zeros((inp_features, 1))
-
+        self.weights = np.zeros((inp_features, 1), dtype=float)
+        self.b = 0.0
     def sigmoid(self, Z):
         A = 1 / (1 + np.exp(Z))
         return A
