472e85743108697ea9b3b49f02079dfa16734502
pog <pooriagolestanipg@gmail.com>
2024-02-14 19:38:49 +0330

    model was added and parameters and forward pass and activation function was implemented and tested

---
+++ model.py
@@ -0,0 +1,19 @@
+import numpy
+import numpy as np
+
+
+class logistic_model():
+    def __init__(self, inp_features):
+        self.weights = np.zeros((inp_features, 1))
+
+    def sigmoid(self, Z):
+        A = 1 / (1 + np.exp(Z))
+        return A
+
+    def forward(self, X):
+        #X is a concated x's horizontally
+
+        Z = np.dot(self.weights.T, X)
+        A = self.sigmoid(Z)
+
+        return A
